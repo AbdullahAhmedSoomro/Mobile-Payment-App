19e1bc1c584a6fe6c9d8db9c2236cb26e5823166
Abdullah <abdullahsoomro245@gmail.com>
2022-06-18 10:46:50 +0500

    Task Complete

--- assingment_6/lib/Constants/image_constants.dart
+++ assingment_6/lib/Constants/image_constants.dart
@@ -10,12 +10,12 @@ class ImageConstant {
   static String image9 = "assets/images/i.png";
   static String image10 = "assets/images/j.png";
   static String image11 = "assets/images/k.png";
-  static String firstbg = 'assets/images/1stbg.png';
-  static String firstimg = 'assets/images/1stpic.png';
-  static String secbg = 'assets/images/2ndbg.png';
-  static String secImg = 'assets/images/2ndpic.png';
-  static String thirdbg = 'assets/images/3rdbg.png';
-  static String thirdimg = 'assets/images/3rdimg.png';
-  static String fourthimg = 'assets/images/4thimg.png';
-  static String fourthbg = 'assets/images/4thbg.png';
+  static String firstbg = "assets/images/l.png";
+  static String firstimg = "assets/images/m.png";
+  static String secbg = "assets/images/n.png";
+  static String secImg = "assets/images/o.png";
+  static String thirdbg = "assets/images/p.png";
+  static String thirdimg = "assets/images/q.png";
+  static String fourthimg = "assets/images/r.png";
+  static String fourthbg = "assets/images/s.png";
 }
